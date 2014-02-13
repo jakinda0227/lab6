fd8b5ee754856688e9932d8f627decf12811b541
vagrant <vagrant@precise32.(none)>
2014-02-13 18:36:55 +0000

    ajax lab

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -26,13 +26,49 @@ function addProjectDetails(e) {
 	// get rid of 'project' from the front of the id 'project3'
 	var idNumber = projectID.substr('project'.length);
 
-	console.log("User clicked on project " + idNumber);
+	var url = '/project/' + idNumber;
+	$.get(url, addProject);
 }
+
+var id;
+
+
+function addProject(response) {
+	id = response['id'];	
+	var query = $('#project' + id).find('p').text();
+	var details = $('#project' + id).find('.' + 'details');
+	var projectHTML =  '<img src="' + response['image'] + '" class="detailsImage">' + 
+	'<p><strong>Due date: </strong>' + response['date'] + '</p>' + 
+	'<p>' + response['summary'] + '</p>';
+
+	
+	details.html(projectHTML);
+
+	
+}
+
 
 /*
  * Make an AJAX call to retrieve a color palette for the site
  * and apply it
  */
 function randomizeColors(e) {
-	console.log("User clicked on color button");
+	e.preventDefault();
+	$.get('/palette', addColors);
+}
+
+function addColors(response) {
+	var colors = response['colors']['hex'];
+	
+	$('body').css('background-color', colors[0]);
+	$('.thumbnail').css('background-color', colors[1]);
+	$('h1, h2, h3, h4, h5, h5').css('color', colors[2]);
+	$('p').css('color', colors[3]);
+	$('.project img').css('opacity', .75);
+}
+
+function processYT(response) {
+	var entries = response['feed']['entry'];
+	var videoId = entries[0]['id']['$t'].substr('http://gdata.youtube.com/feeds/api/videos/'.length);
+	$('#project' + id).find('.details').append('<iframe src="http://www.youtube.com/embed/' + videoId + '"></iframe>');
 }
